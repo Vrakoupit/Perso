51d3282f4829176e7aa1abaf8d90fa2b9007718e
Vincent Curt <vincent.curt@etu-webschoolfactory.fr>
2016-11-12 17:34:38 +0100

    First commit, first files added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vincent Curt</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<nav>
+        <ul>
+            <li><img src="" alt=""></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+    <section class="sectionGauche">
+        <div>
+            <li><h2>Vincent Curt</h2></li>
+            <li><img src="" alt=""></li>
+            <li><p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p></li>
+        </div>
+    </section><!--
+    --><section class="sectionDroite">
+        <div>
+            <li><h2>Le blog</h2></li>
+            <li><img src="" alt=""></li>
+            <li><p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p></li>
+        </div>
+    </section>
+</body>
+</html>
