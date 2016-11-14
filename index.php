51d69f9c0b71e7713e2685a0f612e2af4770b682
Vincent Curt <vincent.curt@etu-webschoolfactory.fr>
2016-11-14 16:37:30 +0100

    Completed home page style, responsive needed, animation needed to go to the blog

--- index.php
+++ index.php
@@ -3,29 +3,41 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Vincent Curt</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body id="home">
 	<nav>
-        <ul>
-            <li><img src="" alt=""></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </nav>
-    <section class="sectionGauche">
-        <div>
-            <li><h2>Vincent Curt</h2></li>
-            <li><img src="" alt=""></li>
-            <li><p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p></li>
-        </div>
-    </section><!--
-    --><section class="sectionDroite">
-        <div>
-            <li><h2>Le blog</h2></li>
-            <li><img src="" alt=""></li>
-            <li><p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p></li>
-        </div>
-    </section>
+		<ul>
+			<li><a href=""><img src="" alt="">LOGO</a></li>
+			<li><a href="">About</a></li>
+			<li><a href="">Blog</a></li>
+		</ul>
+	</nav>
+	<section class="sectionGauche">
+		<div>
+			<ul>
+				<li><h2>Vincent Curt</h2></li>
+				<li><img src="" alt=""></li>
+				<li><p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p></li>
+				<ul id="infosPicto">
+					<li><a href="">Resume</a></li>
+					<li><a href="">Github</a></li>
+					<li><a href="">Twitter</a></li>
+					<li><a href="">Linkedin</a></li>
+				</ul>
+			</ul>
+		</div>
+	</section><!--
+--><section class="sectionDroite" id="blog">
+		<div>
+			<ul>
+				<li><h2>Le blog</h2></li>
+				<li><img src="" alt=""></li>
+				<li><p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p></li>
+				<li><a href="">Découvrir</a></li>
+			</ul>
+		</div>
+	</section>
 </body>
 </html>
