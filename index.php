c6db046777026819906dde6872aab4bc5311cf3d
Vincent Curt <vincent.curt@etu-webschoolfactory.fr>
2016-11-16 14:51:37 +0100

    Fix background and links added

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Vincent Curt</title>
+	<meta name="apple-mobile-web-app-status-bar-style" content="#2c3e50">
+	<meta name="theme-color" content="#2c3e50">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/main.css">
 </head>
@@ -14,7 +16,7 @@
 			<li><a href="/blog">Blog</a></li>
 		</ul>
 	</nav>
-	<section class="sectionGauche">
+	<section class="section sectionGauche">
 		<div>
 			<ul>
 				<li><h2>Vincent Curt</h2></li>
@@ -22,14 +24,14 @@
 				<li><p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p></li>
 				<ul id="infosPicto">
 					<li><a href="">Resume</a></li>
-					<li><a href="">Github</a></li>
-					<li><a href="">Twitter</a></li>
-					<li><a href="">Linkedin</a></li>
+					<li><a href="https://github.com/Vrakoupit">Github</a></li>
+					<li><a href="https://twitter.com/Vrakoupit">Twitter</a></li>
+					<li><a href="https://fr.linkedin.com/in/vincentcurt">Linkedin</a></li>
 				</ul>
 			</ul>
 		</div>
-	</section><!--
---><section class="sectionDroite" id="blog">
+	</section>
+	<section class="section sectionDroite" id="blog">
 		<div>
 			<ul>
 				<li><h2>Le blog</h2></li>
